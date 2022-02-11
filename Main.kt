59f65360edf3e8146d368c3824237f2d94a08b05
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2022-02-11 17:57:49 +0300

    Update Main.kt
    Implemented naive version of sorting number tool.

--- Main.kt
+++ Main.kt
@@ -1,5 +1,32 @@
 package sorting
 
 fun main() {
-    // write your code here
+    val lineNumbers = mutableListOf<String>()
+    do {
+        val input = readLine()
+        if (input != null) {
+            lineNumbers.add(input)
+        }
+    } while (input != null)
+
+    val numbers: MutableList<Int> = mutableListOf()
+    for (line in lineNumbers) {
+        for (number in line.split("\\s+".toRegex()).map { it.toInt() }) {
+            numbers.add(number)
+        }
+    }
+
+    var greatestNumber: Int = Int.MIN_VALUE
+    var counter = 0
+    for (number in numbers) {
+        if (number > greatestNumber) {
+            greatestNumber = number
+            counter = 1
+        } else if (number == greatestNumber) {
+            counter++
+        }
+    }
+
+    println("Total numbers: ${numbers.size}.")
+    println("The greatest number: $greatestNumber (${counter} time(s)).")
 }
