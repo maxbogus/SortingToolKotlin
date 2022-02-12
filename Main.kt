edd5947b85e8f28f4a1b177cb1fe0f230ad83adb
Max Boguslavsky <521205+maxbogus@users.noreply.github.com>
2022-02-12 03:06:03 +0300

    Update Main.kt
    - added draft to handle arguments.

--- Main.kt
+++ Main.kt
@@ -1,16 +1,32 @@
 package sorting
 
-fun main() {
-    val lineNumbers = mutableListOf<String>()
-    do {
-        val input = readLine()
-        if (input != null) {
-            lineNumbers.add(input)
-        }
-    } while (input != null)
+fun main(args: Array<String>) {
+    val type = if (args[0] == "-dataType") {
+        args[1]
+    } else {
+        "word"
+    }
 
+    val lines: MutableList<String> = parseInput()
+
+    when (type) {
+        "long" -> countGreatestNumber(lines)
+        "word" -> findLongestWord(lines)
+        else -> findLongestLine(lines)
+    }
+}
+
+fun findLongestLine(lines: MutableList<String>) {
+    TODO("Not yet implemented")
+}
+
+fun findLongestWord(lines: MutableList<String>) {
+    TODO("Not yet implemented")
+}
+
+private fun countGreatestNumber(lines: MutableList<String>) {
     val numbers: MutableList<Int> = mutableListOf()
-    for (line in lineNumbers) {
+    for (line in lines) {
         for (number in line.split("\\s+".toRegex()).map { it.toInt() }) {
             numbers.add(number)
         }
@@ -29,4 +45,15 @@ fun main() {
 
     println("Total numbers: ${numbers.size}.")
     println("The greatest number: $greatestNumber (${counter} time(s)).")
+}
+
+private fun parseInput(): MutableList<String> {
+    val lines = mutableListOf<String>()
+    do {
+        val input = readLine()
+        if (input != null) {
+            lines.add(input)
+        }
+    } while (input != null)
+    return lines
 }
